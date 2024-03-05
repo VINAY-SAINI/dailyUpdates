8edfefafc1187d95ca2f462a883012e038aef260
Nidhi Dhiman <vinaysaini7988@gmail.com>
2024-03-05 18:36:42 +0530

    workdone

---
+++ work.cs
@@ -0,0 +1,59 @@
+/*
+On 5th March 
+Work assign:-
+Basic Enumeration in C#,
+Classes,
+Encapsulation, 
+Inheritance
+(Write Programs for all types of inheritance)
+Work Done:-
+c# syntax
+c# output
+c# comments
+  -single line
+  -multiline
+c# variable
+c# const
+c# data types
+c# casting
+  -implict casting
+  -explicit casting
+c# user input
+c# operator
+c# strings
+c# boolean
+c# if Else
+c# switch
+c# while loop ,do while
+c# for loop
+c# foreach loop
+c# break, continue
+c# arrays (1d,2D)
+c# methods
+C# overloading
+c# classes
+c# objects
+c# constructors
+c# access modier(still facing issue in namespace and assembly understanding)
+C# properties 
+  -getter,setter
+c# inheritence and types
+c# polymorphism 
+c# abstraction
+  -abstract classes
+c# interfaces 
+c# enums
+c# exception
+  - try and catch
+
+I have implemented all these concept in visual studio code editor 
+ 
+Problems I faced today
+- setting up the environment 
+(resolved) 
+-still didn't understand how namespace and assembly works
+-not able to push the code on github
+
+most of the doubts I resolved through chatgpt and google 
+ 
+ */
