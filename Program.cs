ee1305e6e644fecbc2b9789ac69c32bd17093a5c
Nidhi Dhiman <vinaysaini7988@gmail.com>
2024-03-05 17:38:54 +0530

    adding some more code

--- Program.cs
+++ Program.cs
@@ -288,31 +288,31 @@ Console.WriteLine(str);
 //   }
 // }
 
-class Program 
-{
-  static void Main(string[] args)
-  {
-    DemoClass myObj = new DemoClass();
-    myObj.myMethod();
-    myObj.myOtherMethod();
-  }
+// class Program 
+// {
+//   static void Main(string[] args)
+//   {
+//     DemoClass myObj = new DemoClass();
+//     myObj.myMethod();
+//     myObj.myOtherMethod();
+//   }
 
 
-static void checkAge(int age)
-{
-  if (age < 18)
-  {
-    throw new ArithmeticException("Access denied - You must be at least 18 years old.");
-  }
-  else
-  {
-    Console.WriteLine("Access granted - You are old enough!");
-  }
-}
+// static void checkAge(int age)
+// {
+//   if (age < 18)
+//   {
+//     throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+//   }
+//   else
+//   {
+//     Console.WriteLine("Access granted - You are old enough!");
+//   }
+// }
 
-static void Main(string[] args)
-{
-  checkAge(15);
-}
+// static void Main(string[] args)
+// {
+//   checkAge(15);
+// }
    
-}
+// }
